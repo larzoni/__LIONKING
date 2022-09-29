000f193cc5b979308cd03f1d4a0a6f6a04f61e45
Helena <helena@sixfeetcreative.com>
2022-09-29 13:00:51 +0200

    changes js click

--- thecast.js
+++ thecast.js
@@ -1,17 +1,29 @@
-function simbaInfo() {
-    var x = document.getElementById("mySimba");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+// function simbaInfo() {
+//     var x = document.getElementById("mySimba");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
 
-  function nalaInfo() {
-    var x = document.getElementById("myNala");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+//   function nalaInfo() {
+//     var x = document.getElementById("myNala");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+function toggleSimba() {
+  document.querySelectorAll('.cast-info').forEach(el => {
+    el.classList.remove('visible');
+  });
+
+  let simbaInfo = document.getElementById('mySimba');
+  simbaInfo.classList.toggle('visible');
+}
+
+let simbaButton = document.getElementById('simbaButton');
+simbaButton.addEventListener('click', toggleSimba);
