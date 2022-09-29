7e641712a6cfe592abcb285ea270e635598f3e40
Helena <helena@sixfeetcreative.com>
2022-09-29 15:07:43 +0200

    musicpage draft 1

---
+++ themusic.js
@@ -0,0 +1,40 @@
+
+//CIRCLE OF LIFE
+document.getElementById("imgMovieCircle").onclick = function() {
+    var audio = document.getElementById("audioMovieCircle");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("imgGameCircle").onclick = function() {
+    var audio = document.getElementById("audioGameCircle");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+// HAKUNA MATATA
+document.getElementById("imgMovieHakuna").onclick = function() {
+    var audio = document.getElementById("audioMovieHakuna");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("imgGameHakuna").onclick = function() {
+    var audio = document.getElementById("audioGameHakuna");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+// BE PREPARED
+
+document.getElementById("imgMoviePrepared").onclick = function() {
+    var audio = document.getElementById("audioMoviePrepared");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("imgGamePrepared").onclick = function() {
+    var audio = document.getElementById("audioGamePrepared");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
